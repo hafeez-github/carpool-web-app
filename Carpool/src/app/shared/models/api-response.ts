c8d36c8671c44b45dc00f53c484250e67187f206
hafeez <hafeez.s@Technovert.com>
2023-03-31 11:25:28 +0530

    Deleted old files, starting afresh

--- Carpool/src/app/shared/models/api-response.ts
+++
@@ -1,9 +0,0 @@
-export interface APIResponse<T>{
-    
-    data: T;
-    statusCode:number;
-    status:string;
-    message:any;
-    isSuccess:any;
-
-}
--- Carpool/src/app/shared/models/rideResponse.ts
+++
@@ -1,9 +0,0 @@
-export class RideResponse{
-    id:number=-1;
-    offerId:number=-1;
-    bookingId:number=-1;
-    tripStart:string='';
-    tripEnd:string="";
-    price:number=-1;
-    distance:number=-1;
-}
