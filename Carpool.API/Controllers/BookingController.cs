c6e90f91e326629454ad2ae0e212e73e9190fe9e
hafeez <hafeez.s@Technovert.com>
2023-04-13 14:54:26 +0530

    Used SHA256 for password encryption

--- Carpool.API/Controllers/BookingController.cs
+++ Carpool.API/Controllers/BookingController.cs
@@ -10,11 +10,13 @@ using Carpool.Models.ResponseModels;
 using Carpool.Services;
 using Carpool.Services.Interfaces;
 using Carpool.Utilities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Carpool.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class BookingController : ControllerBase
